c335a6e7ffff8e779aeee933366d3cf869600fa9
Ry-leigh <ryanaarondc@gmail.com>
2025-09-06 03:11:44 +0800

    Add modules export activity

---
+++ 11_javascript-refresher/15_modules_export.js
@@ -0,0 +1,12 @@
+export default function greet() {
+  return "Hello!";
+}
+
+export const myInformation = {
+    firstName: "Ryan Aaron",
+    lastName: "Dela Cruz",
+    age: 20,
+    birthday: "June 10",
+    program: "BS Information Systems",
+    yearLevel: "3rd Year",
+};
