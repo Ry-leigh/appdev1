ef5b7ca7cd3833be6c8ed209e851d5a835a8cc2c
Ry-leigh <ryanaarondc@gmail.com>
2025-09-05 23:07:06 +0800

    Add variables activity

---
+++ 11_javascript-refresher/02_variables.js
@@ -0,0 +1,22 @@
+let name = "Ryan";
+let age = 20;
+let isStudent = true;
+
+console.log("Name:", name)
+console.log("Age:", name)
+console.log("Is Student:", name)
+
+console.log(typeof name)
+console.log(typeof age)
+console.log(typeof isStudent)
+
+let a = 10;
+let b = 3;
+
+console.log("Add:", a + b);       
+console.log("Subtract:", a - b);  
+console.log("Multiply:", a * b);  
+console.log("Divide:", a / b);    
+
+console.log(5 == "5")   
+console.log(5 === "5")
