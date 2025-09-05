6d9ffe49148deee74afc4049724946d485ca67ef
Ry-leigh <ryanaarondc@gmail.com>
2025-09-06 01:24:42 +0800

    Add essential features activity

---
+++ 11_javascript-refresher/08_essential_features.js
@@ -0,0 +1,11 @@
+const hobbies = ["Drawing", "Sleeping", "Playing"];
+hobbies.map(hobby => console.log(hobby));
+
+const student = { name: "Ryan", age: 20 }
+console.log(student.name)
+console.log(student.age)
+
+let numbers = [1, 2, 3];
+let moreNumbers = [...numbers, 4, 5];
+
+console.log(moreNumbers);
