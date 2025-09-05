1858bae355c87e1adcaac7db2e354925d04e4596
Ry-leigh <ryanaarondc@gmail.com>
2025-09-06 02:09:41 +0800

    Add let and const activity

---
+++ 11_javascript-refresher/10_let_const.js
@@ -0,0 +1,20 @@
+let name1 = "Ryan";
+let age1 = 21;
+
+name1 = "Mythrynne";
+age1 = 12;
+
+console.log("let:", name1, age1);
+
+const name2 = "Ryan";
+const age2 = 20;
+// name2 = "Mythrynne";
+// age2 = 12;
+console.log("const:", name2, age2)
+
+var name = "Ryan"; 
+var age = 20;
+name = "Mythrynne";
+age = 12;
+
+console.log("var:", name, age);
