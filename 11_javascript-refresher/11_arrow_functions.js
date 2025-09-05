80d4d91d9c1b04b687f31d80212efc2fb9dee3f2
Ry-leigh <ryanaarondc@gmail.com>
2025-09-06 02:13:02 +0800

    Add arrow functions activity

---
+++ 11_javascript-refresher/11_arrow_functions.js
@@ -0,0 +1,20 @@
+// function greet(name) {
+//   return "Hello, " + name;
+// }
+
+// function square(n) {
+//   return n * n;
+// }
+
+// function sayHi() {
+//   console.log("Hi!");
+// }
+
+const greet = name => `Hello, ${name}`;
+console.log(greet("Ryan"));
+
+const square = n => n * n;
+console.log(square(5));
+
+const sayHi = () => console.log("Hi!");
+sayHi();
