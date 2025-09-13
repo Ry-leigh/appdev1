0363987f0f31418aaa3063c3e9b62860a76ac2dc
Ry-leigh <ryanaarondc@gmail.com>
2025-09-13 10:47:30 +0800

    Done writing markup with JSX

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -9,6 +9,15 @@ function MyButton() {
   );
 }
 
+function AboutPage() {
+  return (
+    <>
+      <h1>About</h1>
+      <p>Hello there.<br />How do you do?</p>
+    </>
+  );
+}
+
 function App() {
     return (
     <div>
