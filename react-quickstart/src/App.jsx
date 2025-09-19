0caf92bdaf905644aeb26e90c97820508c65e05a
Ry-leigh <ryanaarondc@gmail.com>
2025-09-19 21:56:43 +0800

    Done rendering lists

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -36,18 +36,42 @@ function LoginForm() {
   );
 }
 
-let isLoggedIn = true;
-let content;
-if (isLoggedIn) {
-  content = <AdminPanel />;
-} else {
-  content = <LoginForm />;
+// let isLoggedIn = true;
+// let content;
+// if (isLoggedIn) {
+//   content = <AdminPanel />;
+// } else {
+//   content = <LoginForm />;
+// }
+
+const products = [
+  { title: 'Cabbage', isFruit: false, id: 1 },
+  { title: 'Garlic', isFruit: false, id: 2 },
+  { title: 'Apple', isFruit: true, id: 3 },
+];
+
+function ShoppingList() {
+  const listItems = products.map(product =>
+    <li
+      key={product.id}
+      style={{
+        color: product.isFruit ? 'magenta' : 'darkgreen'
+      }}
+    >
+      {product.title}
+    </li>
+  );
+
+  return (
+    <ul>{listItems}</ul>
+  );
 }
+
 
 function App() {
   return (
     <>
-      {content}
+      <ShoppingList />
     </>
   );
 }
