795e0bad48b9592f6095b1ed1ec112c1e3415202
Ry-leigh <ryanaarondc@gmail.com>
2025-10-29 15:37:45 +0800

    Done with how to render multiple components at a time

--- react-describing-ui/src/App.jsx
+++ react-describing-ui/src/App.jsx
@@ -1,15 +1,14 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import TodoList from './components/App'
-import './App.css'
+import List from './components/App'
 
 function App() {
   const [count, setCount] = useState(0)
   
   return (
     <>
-     <TodoList />
+     <List />
     </>
   )
 }
