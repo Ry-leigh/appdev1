ab311347b489632c087f160fde5f39c0c4b50962
Ry-leigh <ryanaarondc@gmail.com>
2025-10-29 15:12:44 +0800

    Done with how to add markup to JavaScript with JSX

--- react-describing-ui/src/App.jsx
+++ react-describing-ui/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import Gallery from './components/Gallery'
+import TodoList from './components/App'
 import './App.css'
 
 function App() {
@@ -9,7 +9,7 @@ function App() {
   
   return (
     <>
-     <Gallery />
+     <TodoList />
     </>
   )
 }
